5b86d4dab2369f78bfd8079d02078072f5ce28cf
MattK <MattK1454@users.noreply.github.com>
2021-01-16 19:35:50 -0600

    statistical analysis of variance added.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,8 @@
 #load needed libraries
 library(dplyr)
 
+#Deliverable 1
+
 #create mechaCar dataframe
 MechaCar_df <- read.csv('MechaCar_mpg.csv')
 
@@ -10,3 +12,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #run summary function on linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_df))
 
+#Deliverable 2
+
+#read in Suspension coil as table
+suspension_coil <- read.csv('Suspension_Coil.csv')
+
+#create total_summary dataframe
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#create lot summary data
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
